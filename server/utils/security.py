7ef5d01a7bee834393329d73456bfa7784d1a8b6
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-12-08 12:55:12 +0530

    Update security.py

--- server/utils/security.py
+++ server/utils/security.py
@@ -3,16 +3,23 @@ from typing import Optional
 from passlib.context import CryptContext
 from jose import jwt
 from sklearn.utils import deprecated
+from cryptography.fernet import Fernet
 
-from server.utils.config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_MINUTES
+from server.utils.config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_MINUTES, ENCRYPTION_KEY
+
+# key = Fernet.generate_key()
+cipher_suite = Fernet(ENCRYPTION_KEY)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -25,3 +32,17 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
     return encoded_jwt
+
+
+def encrypt_message(message: str):
+    encrypted_bytes = cipher_suite.encrypt(message.encode())
+    return encrypted_bytes.decode("utf-8")
+
+
+def decrypt_message(encrypted_message: str):
+    try:
+        decrypted_bytes = cipher_suite.decrypt(encrypted_message.encode())
+        return decrypted_bytes.decode("utf-8")
+    except Exception as e:
+        print(f"[DEBUG] Decryption error: {e}")
+        raise ValueError("Invalid Key or Corrupted Data")
